f601a9394aab21fc8b9a6bcc1a0b4a65e5e9117c
_JXc <64561035+JXCoding@users.noreply.github.com>
2021-12-26 20:01:03 +0100

    Add files via upload

---
+++ API/src/main/java/de/jxson/maventests/api/NMS.java
@@ -0,0 +1,14 @@
+package de.jxson.maventests.api;
+
+import org.bukkit.entity.Player;
+
+/**
+ * Author: Jason M.
+ * de.jxson.maventests.api
+ * 20.12.2021 at 06:29
+ */
+public interface NMS {
+
+    void sendMessage(Player player, String message);
+
+}
